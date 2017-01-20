6464b44506f139dad3ef67614cc43d9756515a80
Steven <stevenhuynh17@gmail.com>
2017-01-19 21:11:22 -0800

    Set up more UI features

--- Assignment_1/src/SalesApp.java
+++ Assignment_1/src/SalesApp.java
@@ -3,36 +3,61 @@ import java.util.*;
 public class SalesApp {
 	public static void main(String[] args) {
 		Scanner user_input = new Scanner(System.in);
-		
 		double totalSales [][][] = new double [3][3][3];
 		// Create a menu for user to select option
-		EnterData(user_input, totalSales);
-		ShowMenu(user_input);
-		// Header showing your project description.
-		// Menu of services (Load data, opt1, opt2, opt3, end)
+		double dummyData [][][] = {
+				{{1.0, 1.0, 1.0}, {2.0, 2.0, 2.0}, {3.0, 3.0, 3.0}}, 
+				{{4.0, 4.0, 4.0}, {5.0, 5.0, 5.0}, {6.0, 6.0, 6.0}}, 
+				{{7.0, 7.0, 7.0}, {8.0, 8.0, 8.0}, {9.0, 9.0, 9.0}}
+		};
+		
+		int option_selection = 0; 
+		
+		while(option_selection != 3) {
+			System.out.printf("The following program allows users to calculate sales for up to 3 products by quarter, geography and per product.\n\n");
+			System.out.printf("Would you like to manually input data or use dummy data?"
+					+ "\n 1) Dummy Data"
+					+ "\n 2) Input Data"
+					+ "\n 3) EXIT"
+					+ "\n SELECT: ");
+			option_selection = user_input.nextInt();
+			
+			if(option_selection == 1) {
+				ShowMenu(user_input, dummyData);
+			} else if(option_selection == 2) {
+				EnterData(user_input, totalSales);
+			} else if(option_selection == 3) {
+				System.out.printf("Goodbye.\n");
+				EndProgram();
+			} else {
+				System.out.printf("Invalid selection. Please try again\n");
+			}
+		}
 	}
 	
-	public static void ShowMenu(Scanner user_input) {
+	public static void ShowMenu(Scanner user_input, double totalSales[][][]) {
 		int option_selection = 0;
 	
-		
-		while(option_selection != 4) {
-			System.out.printf("Welcome, please select one of the following options: "
-					+ "\n1) Product Sales"
-					+ "\n2) Quarter Sales"
-					+ "\n3) Geography Sales"
-					+ "\n4) Exit"
+		while(option_selection != 5) {
+			System.out.printf("\nWelcome, please select one of the following options: "
+					+ "\n 1) Product Sales"
+					+ "\n 2) Quarter Sales"
+					+ "\n 3) Geography Sales"
+					+ "\n 4) Show Data"
+					+ "\n 5) Exit"
 					+ "\n SELECT: ");
 			// What happens if user accidentally input a string?
 			option_selection = user_input.nextInt();
 			
 			if(option_selection == 1) {
-				TotalSalesByProduct();
+				TotalSalesByProduct(user_input, totalSales);
 			} else if(option_selection == 2) {
-				TotalSalesByQuarter();
+				TotalSalesByQuarter(user_input, totalSales);
 			} else if(option_selection == 3) {
-				TotalSalesByGeography();
+				TotalSalesByGeography(user_input, totalSales);
 			} else if(option_selection == 4) {
+				System.out.printf("Current Data: " + Arrays.deepToString(totalSales) + "\n");
+			} else if(option_selection == 5) {
 				break;
 			} else {
 				System.out.printf("Please try again\n\n");
@@ -44,50 +69,126 @@ public class SalesApp {
 	
 	public static void EnterData(Scanner user_input, double[][][] totalSales) {
 		String regions [] = {"West", "North", "South"};
+		String products [] = {"GTX", "Titan", "1080ati"};
+		
+		for(int product=0; product < products.length; product++) {
+			for(int i=0; i < totalSales[0].length; i++) {
+				for(int j=0; j < totalSales[0][0].length; j++) {
+					System.out.printf("Enter total " + products[product] + " sold for region " + regions[j] + " during q" + (i + 1) + ": ");
+					totalSales[product][i][j] = user_input.nextDouble();
+				}
+				System.out.printf("\n");
+			}
+		}
+	}
+	
+	public static void TotalSalesByProduct(Scanner user_input, double totalSales[][][]) {
+		int option_selection = 0;
+		
+		while(option_selection != 4) {
+			System.out.printf("\nWhich product? "
+					+ "\n 1) GTX"
+					+ "\n 2) Titan"
+					+ "\n 3) 1080ati"
+					+ "\n 4) BACK TO MAIN MENU"
+					+ "\n SELECT: ");
+			option_selection = user_input.nextInt();
+			
+			if(option_selection == 1) {
+				System.out.printf(AddByProduct(totalSales, option_selection) + " units\n");
+			} else if(option_selection == 2) {
+				System.out.printf(AddByProduct(totalSales, option_selection) + " units\n");
+			} else if(option_selection == 3) {
+				System.out.printf(AddByProduct(totalSales, option_selection) + " units\n");
+			} else if(option_selection == 4) {
+				break;
+			} else {
+				System.out.printf("Please try again\n\n");
+			}
+		}
+	}
+	
+	public static double AddByProduct(double totalSales[][][], int index) {
+		double total = 0;
+		for(int i=0; i < totalSales[index - 1].length; i++) {
+			for(int j=0; j < totalSales[index - 1][i].length; j++) {
+				total += totalSales[index - 1][i][j];
+			}
+		}
+		return total;
+	}
+	
+	public static void TotalSalesByQuarter(Scanner user_input, double totalSales[][][]) {
+		int option_selection = 0;
+		
+		while(option_selection != 4) {
+			System.out.printf("\nWhich product? "
+					+ "\n 1) q1"
+					+ "\n 2) q1"
+					+ "\n 3) q3"
+					+ "\n 4) BACK TO MAIN MENU"
+					+ "\n SELECT: ");
+			option_selection = user_input.nextInt();
+			
+			if(option_selection == 1) {
+				System.out.printf(AddByQuarter(totalSales, option_selection) + " units\n");
+			} else if(option_selection == 2) {
+				System.out.printf(AddByQuarter(totalSales, option_selection) + " units\n");
+			} else if(option_selection == 3) {
+				System.out.printf(AddByQuarter(totalSales, option_selection) + " units\n");
+			} else if(option_selection == 4) {
+				break;
+			} else {
+				System.out.printf("Please try again\n\n");
+			}
+		}
+	}
+	
+	public static double AddByQuarter(double totalSales[][][], int index) {
+		int total = 0;
 		
 		for(int i=0; i < totalSales[0].length; i++) {
-			System.out.printf("Enter total GTX sold for q" + (i + 1) + ": ");
-			totalSales[0][i][0] = user_input.nextDouble();
+			for(int j=0; j< totalSales[0][0].length; j++) {
+				total += totalSales[index - 1][i][j];
+			}
+		}
+		return total;
+	}
+	
+	public static void TotalSalesByGeography(Scanner user_input, double totalSales[][][]) {
+		int option_selection = 0;
+		
+		while(option_selection != 4) {
+			System.out.printf("\nWhich product? "
+					+ "\n 1) West"
+					+ "\n 2) North"
+					+ "\n 3) South"
+					+ "\n 4) BACK TO MAIN MENU"
+					+ "\n SELECT: ");
+			option_selection = user_input.nextInt();
+			
+			if(option_selection == 1) {
+				
+			} else if(option_selection == 2) {
+				
+			} else if(option_selection == 3) {
+				System.out.printf(AddByGeography(totalSales, option_selection) + " units\n");
+			} else if(option_selection == 4) {
+				break;
+			} else {
+				System.out.printf("Please try again\n\n");
+			}
+		}
+	}
+	
+	public static double AddByGeography(double totalSales[][][], int index) {
+		double total = 0;
+		for(int i=0; i < totalSales[0][0].length; i++) {
 			for(int j=0; j < totalSales[0][0].length; j++) {
-				System.out.printf("Enter total GTX sold for region " + regions[j] + ": ");
-				totalSales[0][i][j] = user_input.nextDouble();
+				total += totalSales[i][j][index - 1];
 			}
-			System.out.printf("\n");
 		}
-		
-		for(int i=0; i < totalSales[0].length; i++) {
-			System.out.printf("Enter total Titan sold for q" + (i + 1) + ": ");
-			totalSales[1][i][0] = user_input.nextDouble();
-			for(int j=0; j < totalSales[1][0].length; j++) {
-				System.out.printf("Enter total Titan sold for region " + regions[j] + ": ");
-				totalSales[1][i][j] = user_input.nextDouble();
-			}
-			System.out.printf("\n");
-		}
-		
-		for(int i=0; i < totalSales[0].length; i++) {
-			System.out.printf("Enter total 1080ati sold for q" + (i + 1) + ": ");
-			totalSales[2][i][0] = user_input.nextDouble();
-			for(int j=0; j < totalSales[2][0].length; j++) {
-				System.out.printf("Enter total 1080ati sold for region " + regions[j] + ": ");
-				totalSales[2][i][j] = user_input.nextDouble();
-			}
-			System.out.printf("\n");
-		}
-		
-		System.out.printf("TOTAL SALES: " + Arrays.deepToString(totalSales) + "\n\n");
-	}
-	
-	public static void TotalSalesByProduct() {
-		System.out.println("1");
-	}
-	
-	public static void TotalSalesByQuarter() {
-		System.out.printf("2");
-	}
-	
-	public static void TotalSalesByGeography() {
-		System.out.printf("3");
+		return total;
 	}
 	
 	public static void EndProgram() {
