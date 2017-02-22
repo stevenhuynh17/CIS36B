278712df6cae51d43643da829ba6c951c180739e
Steven <stevenhuynh17@gmail.com>
2017-02-21 23:13:43 -0800

    Dabbled

---
+++ Udemy_Practice/src/test.java
@@ -0,0 +1,11 @@
+
+public class test {
+	public static void main(String[] args) {
+		byte a1 = 1;
+		short a2 = 2; 
+		int a3 = 3; 
+		long a4 = 50000 + 10 * (a1 + a2 + a3);
+		
+		System.out.println(a4);
+	}
+}
